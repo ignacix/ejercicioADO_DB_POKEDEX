e77c3e9f6126e967f5db62a18bbdd69960b84045
ignacix <ignanj8mail.com>
2023-05-12 16:45:37 -0300

    Agregar archivos de proyecto.

---
+++ Negocio/PokemonNegocio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dominio;
+
+namespace Negocio
+{
+    public class PokemonNegocio
+    {
+        public List<Pokemon> listar()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection con = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+
+            try
+            {
+                con.ConnectionString = "server=.\\SQLEXPRESS; database=POKEDEX_DB ; integrated security=true";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select Numero, Nombre, P.Descripcion, UrlImagen, E.Descripcion as Tipo, D.Descripcion as Debilidad from POKEMONS P, ELEMENTOS E, ELEMENTOS D where P.IdTipo = E.Id and P.IdDebilidad = D.Id";
+                cmd.Connection = con;
+                
+                con.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Numero = (int)dr["Numero"];
+                    aux.Nombre = (string)dr["Nombre"];
+                    aux.Descripcion = (string)dr["Descripcion"];
+                    aux.UrlImagen = (string)dr["UrlImagen"];
+                    aux.Tipo.Descripcion = (string)dr["Tipo"];
+                    aux.Debilidad.Descripcion = (string)dr["Debilidad"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                con.Close(); 
+            }
+        }
+        
+
+        
+
+
+    }
+}
---
+++ PokemonProject/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PokemonProject
+{
+    public partial class Form1 : Form
+    {
+        List<Pokemon> lista;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio PokemonNegocio = new PokemonNegocio();
+            lista = PokemonNegocio.listar();
+            dataGridView1.DataSource = lista;
+            dataGridView1.Columns["UrlImagen"].Visible = false;
+            pictureBox1.Load(lista[0].UrlImagen);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;            
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            Pokemon seleccion = (Pokemon)dataGridView1.CurrentRow.DataBoundItem;
+
+            pictureBox1.Load(seleccion.UrlImagen);
+        }
+    }
+}
